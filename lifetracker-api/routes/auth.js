abb83af22340b768f7b8a5decc54ef61e2e8b3e2
Marley <marleyburrows1@gmail.com>
2023-06-28 16:27:20 -0700

    updated User model

--- lifetracker-api/routes/auth.js
+++ lifetracker-api/routes/auth.js
@@ -4,7 +4,8 @@ const router = express.Router()
 
 router.post("/login", async (req, res, next) =>{
     try {
-        // login info, attempt to authenticate
+       const user = await User.login(req.body)
+       return res.status(200).json({ user })
     } catch (err) {
         next(err)
     }
@@ -12,7 +13,8 @@ router.post("/login", async (req, res, next) =>{
 
 router.post("/register", async (req, res, next) =>{
     try {
-        // create a new user
+        const user = await User.register(req.body)
+        return res.status(201).json({ user })
     } catch (err) {
         next(err)
     }
